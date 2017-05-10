1e8a1befac7904b7ba92c37cd774758017f9b235
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-05-10 14:56:47 +0900

    Define each layers size input/hidden/output

--- kana.m
+++ kana.m
@@ -1,7 +1,18 @@
+%%
+%% This program is based on Coursera Machine Learning ex4
+%%
+
 %% Initialization
 clear ; close all; clc
 
-printf("This is test program\n");
+%% パラメーターを指定する
+input_layer_size  = 2304; % 48x48のサイズの画像
+hidden_layer_size = 802;  % 隠れ層のサイズ、だいぶ適当
+kana_labels       = 73;   % ひらがなは濁音、半濁音含めて73ある
+
+printf("Input Layer size %d \n", input_layer_size);
+printf("Hidden Layer size %d \n", hidden_layer_size);
+printf("Output Layer size %d \n", kana_labels);
 
 fd = fopen('sample.csv', 'r');
 raw = textscan(fd, '%s%s%s', 'delimiter', ',', 'headerLines', 1 );
