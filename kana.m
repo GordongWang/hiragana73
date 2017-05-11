cdf453f5d17fac066f3356a411fe5635383d11bf
hiroyuki nagata <newserver002@gmail.com>
2017-05-11 21:27:10 +0900

    Enable to pick up sample png files

--- kana.m
+++ kana.m
@@ -9,9 +9,10 @@ pkg load strings;
 clear ; close all; clc
 
 %% パラメーターを指定する
-input_layer_size  = 2304; % 48x48のサイズの画像
-hidden_layer_size = 802;  % 隠れ層のサイズ、だいぶ適当
-kana_labels       = 73;   % ひらがなは濁音、半濁音含めて73ある
+input_layer_size  = 48^2; %% 48x48のサイズの画像
+hidden_layer_size = 802;  %% 隠れ層のサイズ、だいぶ適当
+kana_labels       = 73;   %% ひらがなは濁音、半濁音含めて73ある
+sample_size       = 30;   %% それぞれの標本数を30とる
 
 printf("========================================\n");
 printf("=== 入力層、隠れ層、出力層を設定する ===\n");
@@ -20,13 +21,30 @@ printf("Input Layer size %d  \n", input_layer_size);
 printf("Hidden Layer size %d \n", hidden_layer_size);
 printf("Output Layer size %d \n", kana_labels);
 
-fd = fopen('sample.csv', 'r');
-raw = textscan(fd, '%s%s%s', 'delimiter', ',', 'headerLines', 1 );
-fclose(fd);
+X = zeros(kana_labels * sample_size, input_layer_size);
+kanas = glob("./hiragana73/*/");
 
-X = cell2mat(raw);
+for i = 1:rows(kanas),
+  printf("\nPick up from %s %d files.\n ", kanas{i}, sample_size);
+  pngs = glob([kanas{i} "*.png"]);
+
+  for j = 1:sample_size,
+    index = sample_size * (i-1) + j;
+    vec   = imread(pngs{j})(:)';
+    if (columns(vec) > input_layer_size)
+      printf("index: %d, skipping...\n", index);
+      continue;
+    else
+      printf(".");
+    endif
+    %%printf("index: %d, vector row: %d, col: %d\n", index, rows(vec), columns(vec));
+    X(index, :) = vec;
+  endfor
+endfor
+
 m = rows(X);
 
+printf("\n");
 printf("========================================\n");
 printf("=== データセットの数を表示           ===\n");
 printf("========================================\n");
@@ -45,18 +63,6 @@ printf("Theta2 rows: %d, columns: %d\n", rows(Theta2), columns(Theta2));
 
 nn_params = [Theta1(:) ; Theta2(:)];
 
-%% unicode    = cell2mat(raw{1}(1));
-%% filename   = cell2mat(raw{2}(1));
-%% img_base64 = cell2mat(raw{3}(1));
-
-%% save & load temporary file
-%% x  = base64decode(img_base64);
-%% fd = fopen('buffer.png', 'wb');
-%% fwrite(fd, x, 'uint8');
-%% fclose(fd);
-
-%% I = imread('buffer.png');
-
 % Gray Image
-%% colormap(gray);
-%% imshow(I);
+colormap(gray);
+imshow(X);
