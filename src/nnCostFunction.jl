ae799702d4e252ba752e102b03a4e2f905de6476
hiroyuki.nagata <hiroyuki.nagata@veeva.com>
2017-05-16 15:49:15 +0900

    Fix nnCostFunction.jl according to Julia lang syntax

--- src/nnCostFunction.jl
+++ src/nnCostFunction.jl
@@ -18,8 +18,20 @@ function nnCostFunction(nn_params,
 
     # Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
     # for our 2 layer neural network
-    Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), hidden_layer_size, (input_layer_size + 1))
-    Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):length(nn_params)),data_with_labels, (hidden_layer_size + 1))
+    #
+    # reshape (a,m,n)
+    #
+    # reshape ([1, 2, 3, 4], 2, 2)
+    # ->  1  3
+    #     2  4
+    #
+    Theta1 = reshape(nn_params[1:hidden_layer_size * (input_layer_size + 1)]
+                     , hidden_layer_size, (input_layer_size + 1))
+    Theta2 = reshape(nn_params[(1 + (hidden_layer_size * (input_layer_size + 1))):end]
+                     ,data_with_labels, (hidden_layer_size + 1))
+
+    #@printf("Theta1 x: %d, y: %d\n", size(Theta1, 1), size(Theta1, 2))
+    #@printf("Theta2 x: %d, y: %d\n", size(Theta2, 1), size(Theta2, 2))
 
     m = size(X, 1)
 
@@ -29,28 +41,45 @@ function nnCostFunction(nn_params,
 
     y_alt = eye(data_with_labels)
 
-    D1 = zeros(rows(Theta1), columns(Theta1))
-    D2 = zeros(rows(Theta2), columns(Theta2))
+    D1 = zeros(size(Theta1))
+    D2 = zeros(size(Theta2))
 
     # Backpropagation Updates
     for t = 1:m
         # 1, 入力層 a1 にデータセットを入れる
-        act1 = [ones(1,1); X(t,:)']
+        #@printf("Datasets: Rows: %d, Cols: %d \n", size(X[t,:],1), size(X[t,:],2))
+
+        act1 = [ones(1,1); X[t,:]]
         z2   = Theta1 * act1
         act2 = [ones(1,1); sigmoid(z2)]
         z3   = Theta2 * act2
-        act3 = [sigmoid(z3)]
+        # see: http://stackoverflow.com/questions/29159386/how-should-i-convert-a-singleton-array-to-a-scalar
+        # Converting a singleton matrix as a scalar
+        act3 = reshape([sigmoid(z3)], 1)[1]
+
+        #@printf("Act1: Rows: %d, Cols: %d \n", size(act1,1), size(act1,2))
+        #@printf("Act2: Rows: %d, Cols: %d \n", size(act2,1), size(act2,2))
+        #@printf("Act3: Rows: %d, Cols: %d \n", size(act3,1), size(act3,2))
 
         # 目的関数
-        J += - y_alt(:, y(t))' * log(act3) - (1 - y_alt(:, y(t)))' * log(1-act3)
+        #println(- y_alt[:, Int64(y[t])]')
+        #println(log(act3))
+        #println(- (1 - y_alt[:, Int64(y[t])])')
+        #println(log(1-act3))
+
+        J += - y_alt[:, Int64(y[t])]' * log(act3) - (1 - y_alt[:, Int64(y[t])])' * log(1-act3)
         # 2, レイヤー３の出力Kのそれぞれに対して
         # 3, δ2を計算
-        delta_3 = act3 - y_alt(:, y(t))
+        delta_3 = act3 - y_alt[:, Int64(y[t])]
         delta_2 = (Theta2' * delta_3) .* sigmoidGradient([1; z2])
-        delta_2 = delta_2(2:length(rows(deleta_2)), :)
+        delta_2 = delta_2[2:size(delta_2, 1),:]
 
         # 4
         # D1 = D1 + d2 * a1'
+        #@printf("Act1(T): Rows: %d, Cols: %d \n", size(act1',1), size(act1',2))
+        #@printf("Act2(T): Rows: %d, Cols: %d \n", size(act2',1), size(act2',2))
+        #@printf("d2     : Rows: %d, Cols: %d \n", size(delta_2,1), size(delta_2,2))
+        #@printf("d3     : Rows: %d, Cols: %d \n", size(delta_3,1), size(delta_3,2))
         D1 += (delta_2 * act1')
         D2 += (delta_3 * act2')
     end
@@ -58,8 +87,8 @@ function nnCostFunction(nn_params,
     # 目的関数の収束
     J = J / m
 
-    p1 = Theta1(:, 2:length(columns(Theta1))).^2
-    p2 = Theta2(:, 2:length(columns(Theta2))).^2
+    p1 = Theta1[:, 2:size(Theta1,2)].^2
+    p2 = Theta2[:, 2:size(Theta2,2)].^2
     r = sum(sum(p1)) + sum(sum(p2))
     r = r / (2 * m)
     J = J + lambda * r
@@ -67,8 +96,7 @@ function nnCostFunction(nn_params,
     Theta1_grad = D1 / m + lambda * Theta1
     Theta2_grad = D2 / m + lambda * Theta2
 
-    grad = [Theta1_grad(:) ; Theta2_grad(:)]
-    # printf("J = %d\t, grad = %d \n", J, grad)
+    grad = [Theta1_grad[:] ; Theta2_grad[:]]
 
     # multiple values can be returned from a function using tuples
     # if the return keyword is omitted, the last term is returned
